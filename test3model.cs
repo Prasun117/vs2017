e720e4a0f0ce55931c2bee0bcb6e8abb9adfdd96
Prasun117 <57390215+Prasun117@users.noreply.github.com>
2020-03-16 18:05:20 +0530

    model for the controller
    model for the dbcontroller

---
+++ test3model.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWithDba.Models
+{
+    public class Test3
+    {
+        public string Business_Unit { get; set; }
+
+        public string Job_Name { get; set; }
+        public DateTime Scheduled_Date_Time { get; set; }
+        public DateTime Last_Run_Date_Time { get; set; }
+        public DateTime Next_Run_Date_Time { get; set; }
+        public string Scheduled_By_User { get; set; }
+        public string Current_Status { get; set; }
+        public string   Parameter_s { get; set; }
+    public string Performance { get; set; }
+
+    public string Last_3_Runs_Hours { get; set; }
+
+       
+
+
+
+
+
+
+    }
+}
